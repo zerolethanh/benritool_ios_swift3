9b4579fa2b8832b567e9aa666679999a905e15d9
ZEmac <zero.lethanh@gmail.com>
2016-06-28 15:49:06 +0900

    Initial Commit

---
+++ benritool_ios_swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  benritool_ios_swift3
+//
+//  Created by ZEmac on 2016/06/28.
+//  Copyright © 2016年 ZEmac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
